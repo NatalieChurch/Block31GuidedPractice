b21673e1c7425f2752e8fc45adcb2ee589960d98
NatalieChurch <nataliechurch12@gmail.com>
2025-05-08 18:44:31 -0500

    Tried on my own, 6 out of 9 tests passed

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 import express from "express";
+import playlist from "#db/playlist";
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send("You've reached the Playlist API!");
+  });
+
+app.get('/playlist', (req, res) => {
+    res.send(playlist);
+})
+
+app.get('/playlist/:index', (req, res) => {
+    
+    const num = Number(req.params.num)
+    const found = playlist.includes(num)
+    res.status(200).send({exists:found});
+})
 
 export default app
--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 import app from './app.js'
 
+const PORT = 3000; 
+
+app.listen(PORT, () => {
+    console.log(`:rocket: Server is running at http://localhost:${PORT}`);
+  });
